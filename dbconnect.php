b12789ce4eb7228551b5b2a9d82f782b73a32e33
Nurun Decerachmi Emba <92075340+nurundece003@users.noreply.github.com>
2022-05-21 20:52:11 +0700

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connect("localhost","id18964553_root","|i}J)3U-E+td!d~~","id18964553_tokopekita");
+
+if(!$conn){
+	echo "gagal konek database menn";
+} else {
+	
+};
+
+?>
